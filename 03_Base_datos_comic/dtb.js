9508c4b195c01fefbd689a027d59a87bb06d610d
JosephLopB <joshyngppo@gmail.com>
2025-11-05 17:25:06 -0500

    Base datos
    Creación de base de datos Comic

---
+++ 03_Base_datos_comic/dtb.js
@@ -0,0 +1,133 @@
+let comic = [
+    {
+  "nombreComic": "El centurión de la noche",
+  "numeroEp": "3",
+  "genero": "Suspenso",
+  "sinopsis": "Una historia de lucha y resiliencia contada por una voz prodigiosa de nuestro ",
+  "portada": "portada.jpg",
+  "autores": [
+    {
+      "nombre": "Yurani",
+      "edad": 20,
+      "secretIdentity": "Ojalá Batman",
+      "habilidades": ["Programación frond-end", "Edición audiovisual", "Diseño grafico e Ilustración"]
+    },
+    {
+      "name": "Joseph López",
+      "age": 26,
+      "secretIdentity": "Ojalá Peace-Maker",
+      "powers": ["Programación frond-end", "Edición audiovisual", "Diseño grafico e Ilustración", "Automatización de agentes", "creación de bases de datos"]
+    }
+  ],
+  "capitulos": [
+    {
+        "titulo": "La voz del barrio",
+        "portada": "portada.jpg",
+        "sinopsis": "...",
+        "escenas":"5",
+        "escena":[
+            {},
+        ],
+        "personajes": [
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+        ],
+
+    },
+    {
+        "titulo": "la revelión del centurión",
+        "portada": "portada.jpg",
+        "sinopsis": "...",
+        "escenas":"5",
+        "escena":[
+            {},
+        ],
+        "personajes": [
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+        ],
+
+    },
+    {
+        "titulo": "El legado inmortal",
+        "portada": "portada.jpg",
+        "sinopsis": "...",
+        "escenas":"5",
+        "escena":[
+            {},
+        ],
+        "personajes": [
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+            {
+                "nombre":"...",
+                "edad":"...",
+                "rol":"...",
+                "descripción":"",
+            },
+        ],
+
+    },
+  ],
+}
+]
